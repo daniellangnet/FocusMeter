be08541437a362820e6de7575e42fd995b78071d
Daniel Lang <d.lang@flexbit.at>
2012-05-12 17:12:12 +0200

    added configuration class

---
+++ src/FocusMeter/Configuration.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace FocusMeter
+{
+    public class Configuration
+    {
+        public ModifierKeys ShortcutModifierKeys { get; set; }
+        public Keys ShortcutKey { get; set; }
+    }
+}
